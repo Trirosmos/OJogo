7b17e8821a0b444f445bfc9aa012e9bb8a837610
Josué F. Oliveira <josue.feroliveira@gmail.com>
2024-06-02 18:07:18 -0300

    Refactoring

--- main.js
+++ main.js
@@ -134,6 +134,139 @@ let musica = {
 	]
 }
 
+function desenharFundo(canvas, divisoesNotas, compassosPorTela, musica) {
+	let ctx = canvas.getContext("2d");
+	let blockHeight = canvas.height / (divisoesNotas);
+	let tempoPorCompasso = 4 / (musica.bpm / 60);
+	let cOffset = (tempo % tempoPorCompasso)/tempoPorCompasso;
+	let notaStartPos = -cOffset * (canvas.width / compassosPorTela);
+
+	//Cor de fundo
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = "rgb(48,48,48)";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	//Divisões de altura (eixo Y)
+	for(let i = -1; i < divisoesNotas + 1; i++) {
+		ctx.fillStyle = "rgb(59,59,59)";
+		ctx.fillRect(0, canvas.height - blockHeight * i, canvas.width, 2);
+
+		ctx.fillStyle = "white";
+		ctx.fillText(i, 5, canvas.height - blockHeight * i - 4);
+	}
+
+	//Divisões de compasso (eixo X)
+	for(let i = 0; i < 5; i++) {
+		let largura = canvas.width / compassosPorTela; 
+		ctx.fillStyle = "rgb(91,91,91)";
+		ctx.fillRect(notaStartPos + (i * largura) - 1, 0, 3, canvas.height);
+	}
+}
+
+function desenharVoz(canvas, divisoesNotas, compassosPorTela, musica, tempo, numVoz, cor) {
+	let ctx = canvas.getContext("2d");
+	let blockHeight = canvas.height / (divisoesNotas);
+	let semicolcheiaWidth = canvas.width / (compassosPorTela * 16);
+
+	let tempoPorCompasso = 4 / (musica.bpm / 60);
+	let c = tempo > 0 ? Math.floor(tempo / tempoPorCompasso) : -Math.floor((-tempo) / tempoPorCompasso);
+	let cOffset = (tempo % tempoPorCompasso)/tempoPorCompasso;
+	let notaStartPos = -cOffset * (canvas.width / compassosPorTela);
+
+	ctx.fillStyle = cor;
+
+	while(c < musica.compassos.length) {
+		if(c < 0) {
+			c++;
+			notaStartPos += semicolcheiaWidth * 16;
+			continue;
+		}
+
+		let compasso = musica.compassos[c];
+		let voz = compasso.vozes[numVoz];
+
+		let semicolcheiaCounter = 0;
+
+		if(notaStartPos >= canvas.width) {
+			break;
+		}
+
+		for(n in voz.notas) {
+			let nota = voz.notas[n];
+			let duracao = Math.min(16 - semicolcheiaCounter, nota.duracao);
+
+			ctx.save();
+			ctx.globalAlpha = 0.8;
+			ctx.fillRect(notaStartPos, canvas.height - blockHeight * (nota.altura + 1), semicolcheiaWidth * duracao, blockHeight);
+			ctx.restore();
+
+			notaStartPos += semicolcheiaWidth * nota.duracao;
+			semicolcheiaCounter += duracao;
+		}
+
+		c++;
+	}
+}
+
+function desenharPitchDetect(canvas, notas, detectTime, showPeriod, divisoesNotas) {
+	let ctx = canvas.getContext("2d");
+	let windowDiff = (notas.length * detectTime / 1000) - showPeriod;
+
+	let notas_interp = [];
+
+	let lastNote = 0;
+	for(let i = 0; i < notas.length; i++) {
+		let nota = notas[i];
+		if(nota != -1) {
+			lastNote = nota;
+			notas_interp[i] = nota;
+		}
+		else notas_interp[i] = lastNote;
+	}
+
+	notas_interp = iir(notas_interp, 10, true, 1000/detectTime);
+
+	if(windowDiff > 0) {
+		windowDiff *= 1000;
+		windowDiff = Math.round(windowDiff / divisoesNotas);
+		notas.splice(0, windowDiff);
+	}
+
+	let blockWidth = canvas.width / notas.length;
+	let blockHeight = canvas.height / (divisoesNotas);
+
+	ctx.fillStyle = "rgb(100,100, 255)";
+
+	for(let i = 0; i < notas_interp.length; i++) {
+		ctx.fillRect(blockWidth * i, (canvas.height - blockHeight * notas_interp[i]) + blockHeight / 2, blockWidth, 2);
+	}
+}
+
+function iir(buff,cutoff,lowpass, sampleRate) {
+	//https://en.wikipedia.org/wiki/High-pass_filter#Discrete-time_realization
+	//https://en.wikipedia.org/wiki/Low-pass_filter#Discrete-time_realization
+	var rc = 1/(2 * Math.PI * cutoff);
+	var sampleTime = 1/sampleRate;
+	var alpha = lowpass ? sampleTime / (sampleTime + rc) : rc / (sampleTime + rc);
+	
+	if(buff.length < 1) return [];
+
+	var result = Array(buff.length);
+	var lastOut = 0;
+
+	for(x = 0; x < result.length; x++) {
+			if(lowpass) result[x] = lastOut + alpha * (buff[x] - lastOut);
+			else {
+					var lastIn = x === 0 ? 0 : buff[x - 1];
+					result[x] = lastOut * alpha + (buff[x] - lastIn) * alpha
+			}
+
+			lastOut = result[x];
+	}
+
+	return result;
+}
+
 async function handleSuccess(stream) {
 	const context = new AudioContext({latencyHint: "playback"});
 	const source = context.createMediaStreamSource(stream);
@@ -197,7 +330,6 @@ async function handleSuccess(stream) {
 	const tela = document.createElement("canvas");
 	tela.width = window.innerWidth;
 	tela.height = window.innerHeight * 0.9;
-	const ctx = tela.getContext("2d");
 
 	const volume = document.createElement("input");
 	volume.type = "range";
@@ -215,7 +347,6 @@ async function handleSuccess(stream) {
 	document.body.appendChild(tela);
 
 	const notas = [];
-	let notas_interp = [];
 
 	worklet.port.onmessage = function (e) {
 		let nota = e.data;
@@ -231,139 +362,22 @@ async function handleSuccess(stream) {
 	const compassosPorTela = 4;
 	let last = new Date();
 
-	function iir(buff,cutoff,lowpass, sampleRate) {
-    //https://en.wikipedia.org/wiki/High-pass_filter#Discrete-time_realization
-    //https://en.wikipedia.org/wiki/Low-pass_filter#Discrete-time_realization
-    var rc = 1/(2 * Math.PI * cutoff);
-    var sampleTime = 1/sampleRate;
-		var alpha = lowpass ? sampleTime / (sampleTime + rc) : rc / (sampleTime + rc);
-		
-		if(buff.length < 1) return [];
+	function update() {
+		desenharFundo(tela, divisoesNotas, compassosPorTela, musica);
 
-		var result = Array(buff.length);
-    var lastOut = 0;
+		desenharVoz(tela, divisoesNotas, compassosPorTela, musica, tempo, 0, "orange");
+		desenharVoz(tela, divisoesNotas, compassosPorTela, musica, tempo, 1, "blue");
+		desenharPitchDetect(tela, notas, detectTime, showPeriod, divisoesNotas);
 
-    for(x = 0; x < result.length; x++) {
-        if(lowpass) result[x] = lastOut + alpha * (buff[x] - lastOut);
-        else {
-            var lastIn = x === 0 ? 0 : buff[x - 1];
-            result[x] = lastOut * alpha + (buff[x] - lastIn) * alpha
-        }
-
-        lastOut = result[x];
-    }
-
-    return result;
+		window.requestAnimationFrame(update);
 	}
-	
 
-	function atualizarNotas() {
-		let now = new Date();
-		let delta = now - last;
-		let windowDiff = (notas.length * detectTime / 1000) - showPeriod;
-
-		let lastNote = 0;
-		for(let i = 0; i < notas.length; i++) {
-			let nota = notas[i];
-			if(nota != -1) {
-				lastNote = nota;
-				notas_interp[i] = nota;
-			}
-			else notas_interp[i] = lastNote;
-		}
-
-		notas_interp = iir(notas_interp, 10, true, 1000/detectTime);
-
-		if(windowDiff > 0) {
-			windowDiff *= 1000;
-			windowDiff = Math.round(windowDiff / divisoesNotas);
-			notas.splice(0, windowDiff);
-		}
-
-		let blockWidth = tela.width / notas.length;
-		let blockHeight = tela.height / (divisoesNotas);
-		let semicolcheiaWidth = tela.width / (compassosPorTela * 16);
-
-		let numVoz = 0;
-		//let tempo = 0;
-		let tempoPorCompasso = 4 / (musica.bpm / 60);
-		let c = tempo > 0 ? Math.floor(tempo / tempoPorCompasso) : -Math.floor((-tempo) / tempoPorCompasso);
-		let cOffset = (tempo % tempoPorCompasso)/tempoPorCompasso;
-		let notaStartPos = -cOffset * (tela.width / compassosPorTela);
-
-		//Cor de fundo
-		ctx.clearRect(0, 0, tela.width, tela.height);
-		ctx.fillStyle = "rgb(48,48,48)";
-		ctx.fillRect(0, 0, tela.width, tela.height);
-
-		//Divisões de altura (eixo Y)
-		for(let i = -1; i < divisoesNotas + 1; i++) {
-			ctx.fillStyle = "rgb(59,59,59)";
-			ctx.fillRect(0, tela.height - blockHeight * i, tela.width, 2);
-
-			ctx.fillStyle = "white";
-			ctx.fillText(i, 5, tela.height - blockHeight * i - 4);
-		}
-
-		//Divisões de compasso (eixo X)
-		for(let i = 0; i < 5; i++) {
-			let largura = tela.width / compassosPorTela; 
-			ctx.fillStyle = "rgb(91,91,91)";
-			ctx.fillRect(notaStartPos + (i * largura) - 1, 0, 3, tela.height);
-		}
-
-		ctx.fillStyle = "orange";
-
-		while(c < musica.compassos.length) {
-			if(c < 0) {
-				c++;
-				notaStartPos += semicolcheiaWidth * 16;
-				continue;
-			}
-
-			let compasso = musica.compassos[c];
-			let voz = compasso.vozes[numVoz];
-
-			let semicolcheiaCounter = 0;
-
-			if(notaStartPos >= tela.width) {
-				break;
-			}
-
-			for(n in voz.notas) {
-				let nota = voz.notas[n];
-				let duracao = Math.min(16 - semicolcheiaCounter, nota.duracao);
-
-				ctx.save();
-				ctx.fillRect(notaStartPos, tela.height - blockHeight * (nota.altura + 1), semicolcheiaWidth * duracao, blockHeight);
-				ctx.restore();
-
-				notaStartPos += semicolcheiaWidth * nota.duracao;
-				semicolcheiaCounter += duracao;
-			}
-
-			c++;
-		}
-
-		for(let i = 0; i < notas.length; i++) {
-			//ctx.fillRect(blockWidth * i, tela.height - blockHeight * notas[i], blockWidth, blockHeight);
-		}
-
-		/*ctx.fillStyle = "rgb(100,100, 255)";
-
-		for(let i = 0; i < notas_interp.length; i++) {
-			ctx.fillRect(blockWidth * i, (tela.height - blockHeight * notas_interp[i]) + blockHeight / 2, blockWidth, 2);
-		}*/
-
-		window.requestAnimationFrame(atualizarNotas);
-	}
+	update();
 
 	window.addEventListener("resize", function () {
 		tela.width = window.innerWidth;
 		tela.height = window.innerHeight * 0.9;
 	});
-
-	atualizarNotas();
 };
 
 let bot = document.createElement("button");
